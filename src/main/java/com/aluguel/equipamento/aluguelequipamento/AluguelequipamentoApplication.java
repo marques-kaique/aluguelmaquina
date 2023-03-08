b59fc4beea5a5008dfdde44e1d1cfa944369780d
Kaique Marques <63006222+marques-kaique@users.noreply.github.com>
2023-03-07 22:07:47 -0300

    Add files via upload

---
+++ src/main/java/com/aluguel/equipamento/aluguelequipamento/AluguelequipamentoApplication.java
@@ -0,0 +1,13 @@
+package com.aluguel.equipamento.aluguelequipamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelequipamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelequipamentoApplication.class, args);
+	}
+
+}
